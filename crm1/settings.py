71351a0e9ab8eb8d485e4744f6625edf34578fa3
akashgarg98 <garg.5@iitj.ac.in>
2020-05-22 05:05:52 +0530

    added static files

--- crm1/settings.py
+++ crm1/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+#after making css so that it can identify main.css
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]

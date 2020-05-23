b0eff2f363787a205050b21181ddd4a815c433d2
akashgarg98 <garg.5@iitj.ac.in>
2020-05-23 07:11:23 +0530

    added admin panel and set database

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+# now after running python manage.py makemigrations and python maange.py migration we need to register table of customer inthis admin
+from .models import *
+
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+# 1 when we have started to make admin panel
+class Customer(models.Model):
+    name = models.CharField(max_length = 300, null=True)
+    phone = models.CharField(max_length = 300, null=True)
+    email = models.CharField(max_length = 300, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def _str_(self):
+        return self.name
+class Product(models.Model):
+    CATEGORY = (
+        ('indoor','indoor'),
+        ('outdoor','outdoor'),
+    )
+
+
+    name = models.CharField(max_length = 300, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length = 300, null=True,choices=CATEGORY)
+    description = models.CharField(max_length = 300, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+class Order(models.Model):
+    STATUS = (
+        ('pending','pending'),
+        ('out for delivery','out for delivery'),
+        ('delivered','delivered'),
+    )
+
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=300, null=True, choices=STATUS)

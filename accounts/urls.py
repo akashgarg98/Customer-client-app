fceacf00ff3a7b335a007e88ab6a41d31a4e7f93
akashgarg98 <garg.5@iitj.ac.in>
2020-05-27 18:35:14 +0530

    added more features in dynamic url

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,9 +3,9 @@ from . import views
 # when it comes from url of crm here then it triggers these 3 function which takes us to views file
 urlpatterns = [
     # base url
-    path('', views.home),
+    path('', views.home, name="home"),
     # other pages
-    path('products/', views.products),
+    path('products/', views.products,name='products'),
     # <str:pk_tests>/ added this for dynamic url routing i.e we can find customer by id i.e /customer/4/
-    path('customer/<str:pk_test>/', views.customer),
+    path('customer/<str:pk_test>/', views.customer,name="i"),
 ]
